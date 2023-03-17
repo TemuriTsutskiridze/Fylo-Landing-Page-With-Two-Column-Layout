bc39600e8508d16238c96244b4e4d7d7d30409fc
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-03-17 13:16:45 +0400

    event warning fix

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ const button2 = document.getElementById("button2");
 
 const validEmail = /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
 
-button1.addEventListener("click", () => {
+button1.addEventListener("click", (event) => {
     event.preventDefault();
     emailValidation(emailInput1, error1);
 });
 
-button2.addEventListener("click", () => {
+button2.addEventListener("click", (event) => {
     event.preventDefault();
     emailValidation(emailInput2, error2);
 });
